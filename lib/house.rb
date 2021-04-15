9832ae2f2fc05b5a18643a8751b34abcbc13aa20
KristenOrue <oruek@oregonstate.edu>
2021-04-15 10:46:33 -0700

    Add case 6 for the sixth line of the song

--- lib/house.rb
+++ lib/house.rb
@@ -3,13 +3,17 @@ class House
     def line(number)
         case number
         when 1
-        "This is the house that Jack built.\n"
+            "This is the house that Jack built.\n"
         when 2
-        "This is the malt that lay in the house that Jack built.\n"
+            "This is the malt that lay in the house that Jack built.\n"
         when 3
-        "This is the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
-        "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 5
+            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+        when 6
+            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         end
     end
 
