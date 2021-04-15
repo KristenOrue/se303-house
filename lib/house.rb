7b56ca9bb366b40d1880c3d943c3e49e66ef4da6
KristenOrue <oruek@oregonstate.edu>
2021-04-15 10:38:44 -0700

    Add exact expected second verse of the song to the line method

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,12 @@
 class House
 
     def line(number)
+        case number
+        when 1
         "This is the house that Jack built.\n"
+        when 2
+        "This is the malt that lay in the house that Jack built.\n"
+        end
     end
 
 end
