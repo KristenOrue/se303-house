1d50286cd0924c72f6a1ab80777732abdaa21e4e
KristenOrue <oruek@oregonstate.edu>
2021-05-19 16:03:43 -0700

    Create a new conditional for when number==9 in phase method

--- lib/house.rb
+++ lib/house.rb
@@ -19,6 +19,8 @@ class House
             "maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         elsif number == 8
             "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        elsif number == 9
+            "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
             ""
         end
