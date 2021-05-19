ebe48c3c0eeedb86f0fba43b362911196b4b8b23
KristenOrue <oruek@oregonstate.edu>
2021-05-19 15:55:28 -0700

    Create a if number == 6 condition within the phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -13,6 +13,8 @@ class House
             "cat that killed the rat that ate the malt that lay in "
         elsif number == 5
             "dog that worried the cat that killed the rat that ate the malt that lay in "
+        elsif number == 6
+            "cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the"
         else
             ""
         end
