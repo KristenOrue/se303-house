81e84b1f2fee6d9dfdadced5032b4c5983dcc96e
KristenOrue <oruek@oregonstate.edu>
2021-05-19 15:53:55 -0700

    Replace the actual phrase in the line method with the call to phrase

--- lib/house.rb
+++ lib/house.rb
@@ -29,7 +29,7 @@ class House
         when 4
             "This is the #{phrase(number)}the house that Jack built.\n"
         when 5
-            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the #{phrase(number)}the house that Jack built.\n"
         when 6
             "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         when 7
