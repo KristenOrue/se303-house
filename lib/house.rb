80a00b1f20f6f51ce6e78d4a43189be3b71c6082
KristenOrue <oruek@oregonstate.edu>
2021-05-19 16:06:13 -0700

    Add a when number==10 condition to phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -21,6 +21,8 @@ class House
             "man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         elsif number == 9
             "priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        elsif number == 10
+            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
             ""
         end
