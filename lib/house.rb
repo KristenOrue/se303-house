25e8aafba1d33a3d28bd108022467ef42875e67e
KristenOrue <oruek@oregonstate.edu>
2021-05-19 17:40:56 -0700

    Moved verses into an array

--- lib/house.rb
+++ lib/house.rb
@@ -5,32 +5,47 @@ class House
     end
 
     def phrase(number)
-        case number
-        when 1
+        verses = [
+            "the horse and the hound and the horn that belonged to",
+            "the farmer sowing his corn that kept",
+            "the rooster that crowed in the morn that woke",
+            "the priest all shaven and shorn that married",
+            "the man all tattered and torn that kissed",
+            "the maiden all forlorn that milked",
+            "the cow with the crumpled horn that tossed",
+            "the dog that worried",
+            "the cat that killed",
+            "the rat that ate",
+            "the malt that lay in",
             ""
-        when 2
-            "the malt that lay in "
-        when 3
-            "the rat that ate #{phrase(number-1)}"
-        when 4
-            "the cat that killed #{phrase(number-1)}"
-        when 5
-            "the dog that worried #{phrase(number-1)}"
-        when 6
-            "the cow with the crumpled horn that tossed #{phrase(number-1)}"
-        when 7
-            "the maiden all forlorn that milked #{phrase(number-1)}"
-        when 8
-            "the man all tattered and torn that kissed #{phrase(number-1)}"
-        when 9
-            "the priest all shaven and shorn that married #{phrase(number-1)}"
-        when 10
-            "the rooster that crowed in the morn that woke #{phrase(number-1)}"
-        when 11
-            "the farmer sowing his corn that kept #{phrase(number-1)}"
-        when 12
-            "the horse and the hound and the horn that belonged to #{phrase(number-1)}"
-        end
+        ]
+        verses.last(number).join(" ")
+        # case number
+        # when 1
+        #     ""
+        # when 2
+        #     "the malt that lay in "
+        # when 3
+        #     "the rat that ate #{phrase(number-1)}"
+        # when 4
+        #     "the cat that killed #{phrase(number-1)}"
+        # when 5
+        #     "the dog that worried #{phrase(number-1)}"
+        # when 6
+        #     "the cow with the crumpled horn that tossed #{phrase(number-1)}"
+        # when 7
+        #     "the maiden all forlorn that milked #{phrase(number-1)}"
+        # when 8
+        #     "the man all tattered and torn that kissed #{phrase(number-1)}"
+        # when 9
+        #     "the priest all shaven and shorn that married #{phrase(number-1)}"
+        # when 10
+        #     "the rooster that crowed in the morn that woke #{phrase(number-1)}"
+        # when 11
+        #     "the farmer sowing his corn that kept #{phrase(number-1)}"
+        # when 12
+        #     "the horse and the hound and the horn that belonged to #{phrase(number-1)}"
+        # end
     end
 
     def line(number)
