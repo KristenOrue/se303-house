fec007e3d29edb7be91e16cbf54f2250e7a60bc6
KristenOrue <oruek@oregonstate.edu>
2021-05-29 09:54:24 -0700

    Remove old code

--- lib/house.rb
+++ lib/house.rb
@@ -43,20 +43,6 @@ class House
 
     def after_pronouns 
         (0..11).map { |i| "#{nouns[i]}#{verbs[i]}" }
-		# [	
-        #     "the #{nouns[0]} that #{verbs[0]}",
-        #     "the #{nouns[1]} that #{verbs[1]}",
-        #     "the #{nouns[2]} that #{verbs[2]}",
-        #     "the #{nouns[3]} that #{verbs[3]}",
-        #     "the #{nouns[4]} that #{verbs[4]}",
-        #     "the #{nouns[5]} that #{verbs[5]}",
-        #     "the #{nouns[6]} that #{verbs[6]}",
-        #     "the #{nouns[7]} that #{verbs[7]}",
-        #     "the #{nouns[8]} that #{verbs[8]}",
-        #     "the #{nouns[9]} that #{verbs[9]}",
-        #     "the #{nouns[10]} that #{verbs[10]}",
-        #     ""
-		# 	]
 	end
 
     def phrase(number)
