c6bc361d1ecac6cccdb38c5631ffc571e49b62c5
KristenOrue <oruek@oregonstate.edu>
2021-05-28 18:00:45 -0700

    Passes 19 tests now including the sixth line of Pirate

--- test/house_test.rb
+++ test/house_test.rb
@@ -119,4 +119,9 @@ This is the horse and the hound and the horn that belonged to the farmer sowing 
     assert_equal expected, PirateVersion.new.line(5)
   end
 
+  def test_pirate_line_6
+    expected = "Thar be the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+    assert_equal expected, PirateVersion.new.line(6)
+  end
+
 end
